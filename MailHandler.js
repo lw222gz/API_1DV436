d814390f5177bce399145c6c48d30343999ae0cb
lw222gz_1 <lw222gz@student.lnu.se>
2015-12-08 15:29:24 +0000

    Assignment is now complete (I think) depending on awnsers I get from some questions I put on the forum. I removed my Main.js script because I realised I never actually used it.

--- MailHandler.js
+++ MailHandler.js
@@ -1,31 +1,50 @@
 "use strict";
 
 var MailHandler = {
-    mailPath: "Location/",
+    mailPath: "location/",
     
     MailLabels: [],
     
-    setMails: function(){
+    //returns an array of mail objs foreach mail in param @label
+    setMails: function(label){
+        var mailRequest = gapi.client.gmail.users.messages.list({'userId': 'me', 'labelIds': label.id});
+        var mailArr = [];
         
+        mailRequest.execute(function(Response){
+            for (var i = 0; i < Response.messages.length; i++){
+                var getMailDataRequest = gapi.client.gmail.users.messages.get({'userId': 'me', 'id': Response.messages[i].id});
+                
+                getMailDataRequest.execute(function(mailResponse){
+                    mailArr.push({
+                                Snippet: mailResponse.snippet,
+                                Header: mailResponse.payload.headers[16].value
+                                });
+                });
+            }
+        });
+        
+        return mailArr;
     },
     
+    //sets the MailLabels array values. each array position consists of an object containg data about a label and the mails in that label
     setMailLables: function(){
         var googleRequest = gapi.client.gmail.users.labels.list({'userId': 'me'});
         
-        googleRequest.execute(function(request){
-            for (var i = 0; i < request.labels.length; i++){
-                if(request.labels[i].name.indexOf(MailHandler.mailPath) > -1){
-                    MailHandler.MailLabels.push(request.labels[i]);
+        googleRequest.execute(function(response){
+            for (var i = 0; i < response.labels.length; i++){
+                if(response.labels[i].name.indexOf(MailHandler.mailPath) > -1){
+                    MailHandler.MailLabels.push({labelData: response.labels[i], 
+                                                labelMails: MailHandler.setMails(response.labels[i])});
                 }
             }
             
-            //MailHandler.setMails();
-            //console.log(MailHandler.MailLabels);
-            googleMap.placeMarkers();
+            //I need to give each label about 100 miliseconds to load to let mails load or else the labelMails array will be empty.
+            //TODO: better solution
+            setTimeout(function(){
+                googleMap.placeMarkers();
+                
+            },MailHandler.MailLabels.length * 100);
             
-            console.log(MailHandler.MailLabels);
         });
-        
-        
     }
 };
--- googleMap.js
+++ googleMap.js
@@ -1,6 +1,7 @@
 //tutorial I followed on google maps: https://developers.google.com/maps/documentation/javascript/tutorial
 var googleMap = {
     Map:{},
+    //generates the google map
     generateMap: function(){
         Map = new google.maps.Map(document.getElementById("googleMap"), 
                                   {center: {lat: 60, lng:10},
@@ -10,22 +11,23 @@ var googleMap = {
     //geocoder is an api that transforms a location to geographic coordinates
     //src: https://developers.google.com/maps/documentation/geocoding/intro
     placeMarkers: function(){
-        var Geocoder = new google.maps.Geocoder();
-        
-        for (var i = 0; i < MailHandler.MailLabels.length; i++){
-            googleMap.generateGeocode(MailHandler.MailLabels[i], Geocoder);
-        }
-        
-        
+        MailHandler.MailLabels.forEach(function(e){
+            googleMap.generateGeocode(e);
+        });
     },
     
+    //generates the map pin locations
     generateGeocode: function(label, Geocoder){
-        var location = label.name.replace(MailHandler.mailPath, "");
+        
+        var location = label.labelData.name.replace(MailHandler.mailPath, "");
+        
+        var Geocoder = new google.maps.Geocoder();
+        
         Geocoder.geocode({'address': location},
         function(SearchResults, SearchStatus){
             if(SearchStatus === google.maps.GeocoderStatus.OK){
                 //creates a marker on the given position
-                googleMap.createMapMarker(SearchResults[0].geometry.location);
+                googleMap.createMapMarker(SearchResults[0].geometry.location, label);
             }
             else {
                 setTimeout(function(){
@@ -36,9 +38,23 @@ var googleMap = {
         });
     },
     
-    
-    createMapMarker: function(location){
-        var MapMarker = new google.maps.Marker({position: location, map: Map, title: "test"});
+    //creates a map pin at the given location label and set string values for each mail in the label
+    createMapMarker: function(location, label){
+        var MapMarker = new google.maps.Marker({position: location, map: Map});
+        var message = "";
+        
+        for(var i = 0; i < label.labelMails.length; i++){
+            message +=  "<h2>" + label.labelMails[i].Header+ "</h2><br />" + label.labelMails[i].Snippet + "<br/>";
+        }
+        
+        var infoWindow = new google.maps.InfoWindow({
+                content: message
+            });
+            
+        MapMarker.addListener("click", function(){
+                infoWindow.open(Map, MapMarker);
+            });
+        
     }
     
     
